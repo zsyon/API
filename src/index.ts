f7da63674afa23fecdb58cbb9624bc55bf4b6745
Mustafa Kendigüzel <mustafa.kendiguzel@bil.omu.edu.tr>
2023-02-03 23:12:09 +0300

    add dotenv package

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 var app = express();
+const port = process.env.PORT || 3000;
 app.get('/', function (request: any, response: any) {
   response.send('Hello World!');
 });
-app.listen(3000, function () {
-  console.log('Started application on port %d', 3000);
+app.listen(port, function () {
+  console.log(`Started application on port:${port}`);
 });
