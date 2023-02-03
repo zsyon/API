27acdd547fb6e428b6905c8af63a41d730b45a58
Mustafa Kendigüzel <mustafa.kendiguzel@bil.omu.edu.tr>
2023-02-04 01:35:42 +0300

    update some functions

--- src/controllers/auth/login.ts
+++ src/controllers/auth/login.ts
@@ -2,6 +2,8 @@ class AuthController {
   static async loginUser(req: any, res: any, nex: any) {
     res.send('login');
   }
+
+  static async register(req: any, res: any, nex: any) {}
 }
 
 export default AuthController;
